56b655e8ce2938211c6c071f4bfd7e0fdea6bf44
Zoran Đukić <zorandjukic.os@gmail.com>
2021-05-16 22:58:13 +0200

    Update zupanija.sql

--- zupanija.sql
+++ zupanija.sql
@@ -1,6 +1,6 @@
-DROP database IF EXISTS zupanija;
-CREATE database zupanija;
-use zupanija;
+DROP database IF EXISTS zadatak1;
+CREATE database zadatak1;
+use zadatak1;
 
 CREATE TABLE zupanija(
 sifra int NOt NULL PRIMARY key auto_increment,
@@ -37,9 +37,9 @@ insert into zupan (ime,prezime) values
 
 insert into zupanija (naziv,zupan)
 values 
-('Zagrebacka',1,1),
-('Osjecko-Baranjska',2,2),
-('Vukovarsko-Srijemska',3,3);
+('Zagrebacka',1),
+('Osjecko-Baranjska',2),
+('Vukovarsko-Srijemska',3);
 
 insert into opcina (zupanija, naziv)
 values
@@ -52,6 +52,30 @@ values
 
 insert into mjesto (opcina,naziv)
 values
+(1,'Pešæenica'),
+(1,'Zapruðe'),
+(2,'Tvrða'),
+(2,'Uljara'),
+(3,'Sajmište'),
+(3,'Mladost'),
+(1,'Kvatriæ'),
+(1,'Dolac'),
+(2,'Zeleno polje'),
+(2,'Glavna ulica'),
+(3,'Adica'),
+(3,'Centar');
+
+
+update mjesto set naziv='Marko' where sifra=1;
+update mjesto set naziv='Marko' where sifra=2;
+update mjesto set naziv='Marko' where sifra=3;
+update mjesto set naziv='Marko' where sifra=4;
+update mjesto set naziv='Marko' where sifra=5;
+
+select * from zupan;
+select * from zupanija;
+select * from mjesto;
+select * from opcina;
 
 
 
