12886cc67583109ed6789c0b23d76cbc88e97f59
Zoran Đukić <zorandjukic.os@gmail.com>
2021-05-16 22:20:19 +0200

    Playing around with sql

--- zupanija.sql
+++ zupanija.sql
@@ -30,3 +30,33 @@ alter table opcina add foreign key (zupanija) references zupanija(sifra);
 alter table mjesto add foreign key (opcina) references opcina(sifra);
 alter table zupanija add foreign key (zupan) references zupan(sifra);
 
+insert into zupan (ime,prezime) values
+('Marko','Mariæ'),
+('Pero','Periæ'),
+('Miro','Mariæ');
+
+insert into zupanija (naziv,zupan)
+values 
+('Zagrebacka',1,1),
+('Osjecko-Baranjska',2,2),
+('Vukovarsko-Srijemska',3,3);
+
+insert into opcina (zupanija, naziv)
+values
+(1,'Samobor'),
+(1,'Zaprešiæ'),
+(2,'Osijek'),
+(2,'Èepin'),
+(3,'Vukovar'),
+(3,'Vinkovci');
+
+insert into mjesto (opcina,naziv)
+values
+
+
+
+
+
+
+
+
