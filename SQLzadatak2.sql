fab90484796358f4e22357cd9e4e224a7889aa92
Zoran Đukić <zorandjukic.os@gmail.com>
2021-05-21 14:12:05 +0200

    SQLzadatak2 - 2. dio

--- SQLzadatak2.sql
+++ SQLzadatak2.sql
@@ -33,14 +33,10 @@ decko int
 );
 
 create table decko (
-sifra int primary key not null auto_increment,
-haljina varchar(33) not null,
-drugiputa datetime not null,
-suknja varchar(38),
-narukvica int,
-introvertno bit,
-majica varchar(40) not null,
-decko int
+sifra int not null primary key auto_increment,
+indiferentno bit,
+vesta varchar(34),
+asocijalno bit not null
 );
 
 create table deckozarucnica (
@@ -59,12 +55,12 @@ indiferentno bit not null
 );
 
 create table prijatelj (
-sifra int primary key not null auto_increment,
-narukvica int,
-bojakose varchar(37) not null,
-novcica decimal(15, 9),
-lipa decimal(15, 8) not null,
-indiferentno bit not null
+sifra int not null primary key auto_increment,
+modelnaocala varchar(37),
+treciputa datetime not null,
+ekstroventno bit not null,
+prviputa datetime,
+svekar int not null
 );
 
 create table svekar (
@@ -73,6 +69,47 @@ stilfrizura varchar(48),
 ogrlica int not null,
 asocijalno bit not null
 );
+
+alter table brat add foreign key (neprijatelj) references neprijatelj(sifra);
+alter table neprijatelj add foreign key (cura) references cura(sifra);
+alter table cura add foreign key (decko) references decko(sifra);
+alter table deckozarucnica add foreign key (decko) references decko(sifra);
+alter table deckozarucnica add foreign key (zarucnica) references zarucnica(sifra);
+alter table prijatelj add foreign key (svekar) references svekar(sifra);
+
+insert into neprijatelj (haljina,modelnaocala,kuna) values ('plave', 'Police', 1500.00);
+insert into neprijatelj (haljina,modelnaocala, kuna) values ('crvene', 'Kineske', 50.00);
+insert into neprijatelj (haljina,modelnaocala,kuna) values ('crna', 'Nike', 200.00);
+
+insert into cura (haljina,drugiputa,majica) values ('crvena','2020-12-09','adidas');
+insert into cura (haljina,drugiputa,majica) values ('plava', '2019-12-12','nike'); 
+insert into cura (haljina,drugiputa,majica) values ('crna','2015-12-20','adidas');
+
+insert into decko(asocijalno) values (1);
+insert into decko(asocijalno) values (0);
+
+insert into zarucnica(bojakose, lipa,indiferentno) values ('Plava',100.20,1);
+insert into zarucnica(bojakose, lipa,indiferentno) values ('Crna',20.20,0);
+
+
+insert into deckozarucnica(decko,zarucnica) values (1,1);
+insert into deckozarucnica(decko,zarucnica) values (1,2);
+insert into deckozarucnica(decko,zarucnica) values (2,1);
+
+update prijatelj set treciputa='2020-04-30' where sifra>0;
+
+delete from brat where ogrlica not like 14;
+delete from brat where ogrlica=14;
+
+select suknja from cura where drugiputa is null;
+
+
+
+
+
+
+
+
 
 
 
